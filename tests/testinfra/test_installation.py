273c5677d6e7799f417caa3d045901137d77b26b
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-10-24 01:15:30 +0200

    WIP - Update testing

---
+++ tests/testinfra/test_installation.py
@@ -0,0 +1,15 @@
+"""
+Role tests
+"""
+import pytest
+
+# To run all the tests on given docker images:
+pytestmark = pytest.mark.docker_images(
+    'infopen/ubuntu-trusty-ssh:0.1.0',
+    'infopen/ubuntu-xenial-ssh-py27:0.2.0'
+)
+
+
+def test_packages(SystemInfo, Package):
+
+    assert Package('ntp').is_installed
