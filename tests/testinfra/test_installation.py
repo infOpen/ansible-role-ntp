8132a4320425045b3642fa6a1d1343d1a4aa6463
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-10-26 01:20:08 +0200

    Add Updated docker image with iproute2 package

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -6,7 +6,7 @@ import pytest
 # To run all the tests on given docker images:
 pytestmark = pytest.mark.docker_images(
     'infopen/ubuntu-trusty-ssh:0.1.0',
-    'infopen/ubuntu-xenial-ssh-py27:0.2.0'
+    'infopen/ubuntu-xenial-ssh-py27:develop'
 )
 
 
