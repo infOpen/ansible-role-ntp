240a0e017c2fea127e0e30bae0fb3c0a97435853
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-08-21 17:46:33 +0200

    Remove legacy files and tests

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -7,9 +7,6 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+def test_packages(host):
 
-
-def test_foo_b(User):
-    assert User().name == 'root'
+    assert host.package('ntp').is_installed
